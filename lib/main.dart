cfa19af2cc7cd8a8b924d6f08fc25559b24a3611
Ahmed Saeed <ahmed.saeed@ropstam.com>
2022-09-12 16:30:06 +0500

    UI completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,76 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:state_management_examples/views/tasks_screen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [],
-      child: MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: MyText(),
-          ),
-          body: Level1(),
-        ),
-      ),
+    return MaterialApp(
+      home: TasksScreen(),
+      theme: ThemeData(
+          primaryColor: Colors.lightBlueAccent,
+          scaffoldBackgroundColor: Colors.lightBlueAccent),
     );
-  }
-}
-
-class Level1 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Level2(),
-    );
-  }
-}
-
-class Level2 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        MyTextField(),
-        Level3(),
-      ],
-    );
-  }
-}
-
-class Level3 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Text(Provider.of<Data>(context).data);
-  }
-}
-
-class MyText extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Text(Provider.of<Data>(context, listen: false).data);
-  }
-}
-
-class MyTextField extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return TextField(
-      onChanged: (newText) {
-        Provider.of<Data>(context).changeString(newText);
-      },
-    );
-  }
-}
-
-class Data extends ChangeNotifier {
-  String data = 'Some data';
-
-  void changeString(String newString) {
-    data = newString;
-    notifyListeners();
   }
 }
---
+++ lib/views/tasks_screen.dart
@@ -0,0 +1,229 @@
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class TasksScreen extends StatelessWidget {
+  TasksScreen({super.key});
+
+  List<Map<String, dynamic>>? listMapTasks = [
+    {
+      'title': 'Task number 1',
+      'isDone': false,
+    },
+    {
+      'title': 'Task number 2',
+      'isDone': false,
+    },
+    {
+      'title': 'Task number 3',
+      'isDone': false,
+    },
+  ];
+
+  TextEditingController addTaskController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => showModal(context),
+        child: Icon(Icons.add),
+        elevation: 0,
+        backgroundColor: Colors.lightBlueAccent,
+      ),
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // =================================================================
+            // Heading Area
+            // =================================================================
+            Expanded(
+              flex: 1,
+              child: Padding(
+                padding: const EdgeInsets.only(
+                    top: 60.0, left: 30, bottom: 60, right: 30),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // =================================================================
+                    // Icon Button
+                    // =================================================================
+                    CircleAvatar(
+                      maxRadius: 30,
+                      backgroundColor: Colors.white,
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.list,
+                          size: 30,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    // =================================================================
+                    // Heading
+                    // =================================================================
+                    Text(
+                      'Todoey',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 50,
+                        fontWeight: FontWeight.w900,
+                      ),
+                    ),
+                    // =================================================================
+                    // Sub Heading
+                    // =================================================================
+                    Text(
+                      '12 Tasks',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            // =================================================================
+            // Tasks Area
+            // =================================================================
+            Expanded(
+              flex: 2,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(25),
+                    topRight: Radius.circular(25),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.blue.withOpacity(0.4),
+                      spreadRadius: 10,
+                      blurRadius: 30,
+                      offset: Offset(0, -10),
+                    ),
+                  ],
+                ),
+                child: ListView.builder(
+                  physics: NeverScrollableScrollPhysics(),
+                  padding: EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+                  itemCount: listMapTasks?.length ?? 0,
+                  itemBuilder: (context, index) =>
+                      taskItem(context, listMapTasks![index]),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  showModal(context) {
+    showModalBottomSheet<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          height: MediaQuery.of(context).size.height * 0.5,
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          color: Colors.white,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                // =============================================================
+                // Heading
+                // =============================================================
+                Text(
+                  'Add Task',
+                  style: TextStyle(
+                    fontSize: 40,
+                    color: Colors.lightBlueAccent,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                // =============================================================
+                // Text Field
+                // =============================================================
+                TextField(
+                  controller: addTaskController,
+                  textAlign: TextAlign.center,
+                  cursorColor: Colors.lightBlueAccent,
+                  decoration: InputDecoration(
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide:
+                          BorderSide(width: 3, color: Colors.lightBlueAccent),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide:
+                          BorderSide(width: 3, color: Colors.lightBlueAccent),
+                    ),
+                    border: UnderlineInputBorder(
+                      borderSide:
+                          BorderSide(width: 3, color: Colors.lightBlueAccent),
+                    ),
+                  ),
+                ),
+                // =============================================================
+                // Button
+                // =============================================================
+                GestureDetector(
+                  onTap: () {
+                    if (addTaskController.text.isNotEmpty)
+                      addTask(addTaskController.text.trim(), context);
+                  },
+                  child: Container(
+                    margin: EdgeInsets.symmetric(horizontal: 50, vertical: 30),
+                    height: 60,
+                    width: double.infinity,
+                    color: Colors.lightBlueAccent,
+                    child: Center(
+                      child: Text(
+                        'Add',
+                        style: TextStyle(color: Colors.white, fontSize: 18),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  void addTask(String task, context) {
+    listMapTasks?.add({'title': task, 'isDone': false});
+    (context as Element).markNeedsBuild();
+    Navigator.pop(context);
+  }
+
+  ListTile taskItem(context, Map<String, dynamic> item) {
+    return ListTile(
+      title: Text(
+        item['title'],
+        style: TextStyle(
+            decoration: item['isDone']
+                ? TextDecoration.lineThrough
+                : TextDecoration.none,
+            fontSize: 14,
+            color: Colors.black,
+            fontWeight: FontWeight.w600),
+      ),
+      trailing: Checkbox(
+        activeColor: Colors.lightBlueAccent,
+        value: item['isDone'],
+        onChanged: (bool? val) {
+          item['isDone'] = val ?? false;
+          (context as Element).markNeedsBuild();
+        },
+      ),
+    );
+  }
+}
